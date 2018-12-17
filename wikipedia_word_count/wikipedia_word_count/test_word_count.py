2b48a1a17bbb556dd9184d80684516f7695a659c
Wil Bruins-Koetsier <wbkoetsier@users.noreply.github.com>
2018-12-17 17:12:53 +0100

    Fetch pages async using async/await and aiohttp

--- wikipedia_word_count/wikipedia_word_count/test_word_count.py
+++ wikipedia_word_count/wikipedia_word_count/test_word_count.py
@@ -1,17 +1,21 @@
 import unittest
-from .word_count import word_ranking, get_wikipedia_page, parse_wiki_page
+from .word_count import get_most_common_words
+from .word_count import get_wikipedia_page, parse_wikipedia_page
+from .word_count import gather_parsed_wikipedia_pages_by_title, get_wikipedia_pages_by_title
+from .word_count import WAIT_FOR_CONNECTION_CLOSE
+import aiohttp
+import asyncio
 
 
-@unittest.skip
-class TestWordCount(unittest.TestCase):
+class TestGetMostCommonWords(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         """Call the word count on a list of titles for all tests once - call is expensive"""
         titles = ['Monty Python and the Holy Grail', 'there is no such page with this title', 'Monty Python',
-                  'Terry Gilliam', 'Application_programming_interface', 'Robotic process automation',
+                  'Terry Gilliam', '', 'Application_programming_interface', 'Robotic process automation',
                   'IBM_Spectrum_Scale', 'William Hartnell']
-        cls.ranking = word_ranking(titles)
+        cls.ranking = get_most_common_words(titles)
 
     def test_returns_word_ranking_as_list_of_tuples(self):
         self.assertIsInstance(self.ranking, list)
@@ -31,47 +35,106 @@ class TestWordCount(unittest.TestCase):
 
 class TestGetSingleWikipediaPage(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.title = 'Monty Python and the Holy Grail'
-        cls.wmpage = get_wikipedia_page(cls.title)
+    async def run_get_wikipedia_page(self, title: str) -> dict:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            return await get_wikipedia_page(session, title)
+
+    async def run_parse_wikipedia_page(self, title: str) -> str:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            return await parse_wikipedia_page(session, title)
+
+    def setUp(self):
+        # see also: https://stackoverflow.com/a/23642269/4703154
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        self.loop = asyncio.get_event_loop()
 
     def test_get_wikipedia_page_returns_contents_as_json(self):
         """Test that the contents of a page are fetched and returned as wm json"""
-        self.assertIsInstance(self.wmpage, dict)
+        title = 'Monty Python and the Holy Grail'
+        wmpage = self.loop.run_until_complete(self.run_get_wikipedia_page(title))
+        self.assertIsInstance(wmpage, dict)
         # the json dict should contain the following fields, field 'warnings' is optional
         for field in ['continue', 'query']:
-            self.assertIn(field, self.wmpage)
+            self.assertIn(field, wmpage)
         # query field has one key: pages, which itself is a dict with one key, a page id
         # that page id dict contains the pageid, ns, title and revisions
-        for pageid, page in self.wmpage.get('query', {}).get('pages', {}).items():
+        for pageid, page in wmpage.get('query', {}).get('pages', {}).items():
             self.assertIsInstance(page, dict)
-            self.assertEqual(self.title, page.get('title', ''))
+            self.assertEqual(title, page.get('title', ''))
             self.assertIn('revisions', page)
 
-    # @unittest.skip('expensive')
     def test_get_wikipedia_page_accepts_empty_title(self):
-        r = get_wikipedia_page('')
-        self.assertIsInstance(r, dict)
-        self.assertIn('batchcomplete', r)
-        self.assertFalse(r.get('query', {}).get('pages', {}).get('revisions'))
+        wmpage = self.loop.run_until_complete(self.run_get_wikipedia_page(''))
+        self.assertIsInstance(wmpage, dict)
+        self.assertIn('batchcomplete', wmpage)
+        self.assertFalse(wmpage.get('query', {}).get('pages', {}).get('revisions'))
 
     def test_parse_contents_returns_empty_string_on_title_does_not_exist(self):
-        title = 'there is no such page with this title'
-        r = get_wikipedia_page(title)
-        self.assertIsInstance(r, dict)
-        self.assertIn('batchcomplete', r)
-        c = parse_wiki_page(r, title)
+        c = self.loop.run_until_complete(self.run_parse_wikipedia_page('there is no such page with this title'))
         self.assertIsInstance(c, str)
         self.assertFalse(c)
 
     def test_parse_contents_returns_empty_string_on_empty_title(self):
-        r = get_wikipedia_page('')
-        c = parse_wiki_page(r, '')
+        c = self.loop.run_until_complete(self.run_parse_wikipedia_page(''))
         self.assertIsInstance(c, str)
         self.assertFalse(c)
 
     def test_parse_contents_returns_non_empty_string_on_existing_title(self):
-        c = parse_wiki_page(self.wmpage, self.title)
+        c = self.loop.run_until_complete(self.run_parse_wikipedia_page('Monty Python and the Holy Grail'))
         self.assertIsInstance(c, str)
         self.assertTrue(c)  # non-empty string
+
+    def tearDown(self):
+        self.loop.run_until_complete(asyncio.sleep(WAIT_FOR_CONNECTION_CLOSE))
+        self.loop.close()
+
+
+class TestGatherPages(unittest.TestCase):
+
+    async def run_gather_parsed_wikipedia_pages_by_title(self, titles):
+        return await gather_parsed_wikipedia_pages_by_title(titles)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.titles = ['Monty Python and the Holy Grail', 'there is no such page with this title', 'Monty Python',
+                      'Terry Gilliam', '', 'Application_programming_interface', 'Robotic process automation',
+                      'IBM_Spectrum_Scale', 'William Hartnell']
+
+    def setUp(self):
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        self.loop = asyncio.get_event_loop()
+        self.parsed_pages = self.loop.run_until_complete(self.run_gather_parsed_wikipedia_pages_by_title(self.titles))
+
+    def test_gather_pages_returns_list(self):
+        self.assertIsInstance(self.parsed_pages, list)
+
+    def test_gather_pages_returns_list_of_same_length_as_titles(self):
+        self.assertEqual(len(self.titles), len(self.parsed_pages))
+
+    def test_gather_pages_returns_list_of_strings(self):
+        self.assertEqual(len(self.parsed_pages),
+                         len([p for p in self.parsed_pages if isinstance(p, str)]))
+
+    def tearDown(self):
+        self.loop.run_until_complete(asyncio.sleep(WAIT_FOR_CONNECTION_CLOSE))
+        self.loop.close()
+
+
+class TestGetPagesByTitle(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.titles = ['Monty Python and the Holy Grail', 'there is no such page with this title', 'Monty Python',
+                      'Terry Gilliam', '', 'Application_programming_interface', 'Robotic process automation',
+                      'IBM_Spectrum_Scale', 'William Hartnell']
+        cls.parsed_pages = get_wikipedia_pages_by_title(cls.titles)
+
+    def test_get_pages_returns_list(self):
+        self.assertIsInstance(self.parsed_pages, list)
+
+    def test_get_pages_returns_list_of_same_length_as_titles(self):
+        self.assertEqual(len(self.titles), len(self.parsed_pages))
+
+    def test_get_pages_returns_list_of_strings(self):
+        self.assertEqual(len(self.parsed_pages),
+                         len([p for p in self.parsed_pages if isinstance(p, str)]))
--- wikipedia_word_count/wikipedia_word_count/word_count.py
+++ wikipedia_word_count/wikipedia_word_count/word_count.py
@@ -1,26 +1,35 @@
-from typing import Union, List, Tuple
-import requests
+from typing import List
 import mwparserfromhell
+import aiohttp
+import asyncio
+import time
+from collections import Counter
 
 
+TRANSLATION_TABLE = str.maketrans('', '', '()/\\.,;:\'\"*&-')
 API_URL = "https://en.wikipedia.org/w/api.php"
+CONCURRENCY = 10  # asyncio.Semaphore defaults to 1
+# When closing event loop, wait x seconds for the underlying SSL connections to close
+# see https://aiohttp.readthedocs.io/en/stable/client_advanced.html#graceful-shutdown
+WAIT_FOR_CONNECTION_CLOSE = 0.250
 
 
-def word_ranking(titles: List=[]) -> List[Tuple[str, int]]:
-    return [('word', 10), ('the', 1)]
-
-
-def get_wikipedia_page(title: str="") -> dict:
+async def get_wikipedia_page(session: aiohttp.ClientSession, title: str) -> dict:
+    """This coroutine returns the response data for the given Wikipedia page title (json)"""
     params = {"action": "query", "prop": "revisions", "rvlimit": 1,
               "rvprop": "content", "format": "json", "titles": title}
-    r = requests.get(API_URL, params=params)
-    if not r.status_code == 200:
-        raise ValueError(f"Unable to fetch page '{title}', WikiMedia responded with a {r.status_code}")
-    return r.json()
+    print(f'start fetch {title}')
+    start = time.time()
+    async with asyncio.Semaphore(CONCURRENCY), session.get(url=API_URL, params=params) as response:
+        assert response.status == 200
+        data = await response.json()
+    print('fetching {} took {:.2f} seconds'.format(title, time.time() - start))
+    return data
 
 
-def parse_wiki_page(wmpage: dict, title: str) -> str:
+async def parse_wikipedia_page(session: aiohttp.ClientSession, title: str) -> str:
     """Extract the text contents of the given wikipedia page"""
+    wmpage = await get_wikipedia_page(session, title)
     pages = wmpage.get('query', {}).get('pages', {})
     # rvlimit is 1, so there'll be 1 page/revision
     (page_id, page) = pages.popitem() if pages else (-1, None)
@@ -35,3 +44,57 @@ def parse_wiki_page(wmpage: dict, title: str) -> str:
         return ''
     parsed_content = mwparserfromhell.parse(page_content)
     return parsed_content.strip_code(collapse=False)
+
+
+async def gather_parsed_wikipedia_pages_by_title(titles: List[str]) -> List[dict]:
+    """This coroutine fetches results for every title in the list and returns the results as a list.
+
+    Sort order is preserved (see also the docs on asyncio.gather and https://github.com/python/asyncio/issues/432).
+    """
+    print('start get')
+    start = time.time()
+    # the session shouldn't be created outside of a coroutine so it needs to be here rather than in a constructor
+    # see also https://github.com/aio-libs/aiohttp/issues/2473
+    async with aiohttp.ClientSession(raise_for_status=True) as session:
+        # create coroutines for fetching each url
+        coroutines = (parse_wikipedia_page(session=session, title=title) for title in titles)
+        # gather the responses, add any exceptions to the list instead of raising
+        responses = await asyncio.gather(*coroutines, return_exceptions=True)
+    print('gathering all pages took {:.2f} seconds'.format(time.time() - start))
+    return responses
+
+
+def get_wikipedia_pages_by_title(titles: List[str]) -> List[str]:
+    """Set up event loop, run gatherer with the list of titles, close event loop"""
+    # make sure the event loop is always a new one, because we can't reopen an already closed loop and also we don't
+    # want to restart the interpreter between calls to parallel.my_method
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(gather_parsed_wikipedia_pages_by_title(titles))
+    loop.run_until_complete(asyncio.sleep(WAIT_FOR_CONNECTION_CLOSE))
+    loop.close()
+    return result
+
+
+def count_words(text: str) -> Counter:
+    """Return a dictionary of word: #occurences as found in the text."""
+    trans = text.translate(TRANSLATION_TABLE)
+    cntr = Counter()
+    for word in trans.split():
+        cntr[word.lower()] += 1
+    return cntr
+
+
+def get_most_common_words(titles: List[str], top: int=10) -> list:
+    """Return a list of the most common words as found in the Wikipedia pages with the given titles"""
+    text = '\n'.join(get_wikipedia_pages_by_title(titles))
+    cntr = count_words(text)
+    return cntr.most_common(top)
+
+
+if __name__ == '__main__':
+    titles = ['Monty Python and the Holy Grail', 'there is no such page with this title', 'Monty Python',
+              'Terry Gilliam', '', 'Application_programming_interface', 'Robotic process automation',
+              'IBM_Spectrum_Scale', 'William Hartnell']
+    ranking = get_most_common_words(titles, 10)
+    print('ranking:', ranking)
