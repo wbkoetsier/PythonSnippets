1304b3e3cbd0f5c9de5f83445351cc083fdb9f77
Wil Koetsier <wbkoetsier@users.noreply.github.com>
2021-03-12 17:01:53 +0100

    Update format names kata using unpacking operator

--- codewars/kata/kyu6/format_name_string.py
+++ codewars/kata/kyu6/format_name_string.py
@@ -1,14 +1,9 @@
 # https://www.codewars.com/kata/53368a47e38700bd8300030d
 
 def namelist(names):
-    names_list = [n['name'] for n in names]
-    if len(names_list) > 1:
-        return ', '.join(names_list[:-1]) + ' & ' + names_list[-1]
-    elif len(names_list) == 1:
-        return names_list[-1]
+    if not names: return ''
+    *first_names, last_name = iter(names)
+    if first_names:
+        return ', '.join([n['name'] for n in first_names]) + ' & ' + last_name['name']
     else:
-        return ''
-
-
-if __name__ == '__main__':
-    print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]))
+        return last_name['name']
